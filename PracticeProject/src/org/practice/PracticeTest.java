75eac8ed98a358bfb70b8b6c7484e96be9b076b8
sbalan1895 <sbalan1895@gmail.com>
2025-05-25 23:30:59 +0530

    Practice

--- PracticeProject/src/org/practice/PracticeTest.java
+++ PracticeProject/src/org/practice/PracticeTest.java
@@ -26,7 +26,7 @@ public class PracticeTest {
 
 		String input = "Siva";
 
-//		printNonRepetitiveCharacters(input);
+		printNonRepetitiveCharacters(input);
 	}
 
 	public static void printNonRepetitiveCharacters(String input) {
